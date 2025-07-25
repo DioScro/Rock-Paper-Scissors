1485836dd8a939c1f0dcce4ef22b540bd4229c05
DioScro <54253321+caiolahr2@users.noreply.github.com>
2025-07-25 00:46:26 -0300

    Computer choice - done.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log ("JavaScript is linked!");
+function getComputerChoice() {
+    let a = (Math.floor (Math.random() * 3));
+
+    if (a === 0) {
+        console.log ("Rock");
+    } else if (a === 1) {
+        console.log ("Paper");
+    } else {
+        console.log ("Scissors");
+    }
+    
+}
+  (getComputerChoice())
+
+
+
